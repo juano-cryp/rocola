1737e68d99d34935d32a635958f5f76def99f9c0
juano-cryp <ricojuancamilo27@gmail.com>
2026-04-07 22:10:49 -0500

    Update song list and improve song selection logic

--- scrip.js
+++ scrip.js
@@ -6,15 +6,15 @@ const canciones = [
         letra: "Letra de la canción 1..."
     },
     {
-        titulo: "Canción 2",
-        archivo: "Audio/cancion2.mp3",
-        imagen: "Imagenes/cancion2.jpg",
+        titulo: "A mi",
+        archivo: "Audio/AMI.mp3",
+        imagen: "Imagenes/Ami.jpg",
         letra: "Letra de la canción 2..."
     },
     {
-        titulo: "Canción 3",
-        archivo: "Audio/cancion1.mp3",
-        imagen: "Imagenes/cancion1.jpg",
+        titulo: "Back in black",
+        archivo: "Audio/back.mp3",
+        imagen: "Imagenes/BACKINBLACK.jpg",
         letra: "Letra de la canción 3..."
     },
     {
@@ -25,15 +25,50 @@ const canciones = [
     }
 ];
 
+const audio = document.getElementById("audio");
+const menu = document.getElementById("menu");
+
+let actual = 0;
+
 function cambiarCancion(index) {
-    const audio = document.getElementById("audio");
-    const source = document.getElementById("source");
-    audio.pause();
+    actual = index;
+
+    audio.src = canciones[index].archivo;
+
     document.getElementById("titulo").innerText = canciones[index].titulo;
     document.getElementById("portada").src = canciones[index].imagen;
     document.getElementById("letra").innerText = canciones[index].letra;
 
-    source.src = canciones[index].archivo;
-    audio.load();
     audio.play();
+
+    document.querySelectorAll(".item-cancion").forEach(item => {
+        item.classList.remove("activa");
+    });
+
+    document.querySelectorAll(".item-cancion")[index].classList.add("activa");
 }
+
+
+audio.addEventListener("ended", () => {
+    actual++;
+    if (actual >= canciones.length) actual = 0;
+    cambiarCancion(actual);
+});
+
+function cargarMenu() {
+    canciones.forEach((cancion, index) => {
+        const item = document.createElement("div");
+        item.classList.add("item-cancion");
+
+        item.innerHTML = `
+            <img src="${cancion.imagen}">
+            <span>${cancion.titulo}</span>
+        `;
+
+        item.onclick = () => cambiarCancion(index);
+
+        menu.appendChild(item);
+    });
+}
+
+cargarMenu();
